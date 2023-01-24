cf50ea322b04cf0165013a9c6e8dd476ea618200
Lietefat <122794052+Lietefat@users.noreply.github.com>
2023-01-24 11:27:53 -0300

    Add files via upload

---
+++ cli.js
@@ -0,0 +1,16 @@
+import chalk from "chalk";
+import pegaArquivo from "./index.js";
+import validaURL from "./http-validacao.js";
+
+const caminho = process.argv;
+
+async function processaTexto(caminhoDoArquivo){
+    const resultado = await pegaArquivo(caminhoDoArquivo[2]);
+    
+    if (caminho[3] === "validar"){
+        console.log(chalk.yellow("lista de Validados"),await validaURL(resultado));
+    } else{
+        console.log(chalk.yellow ("lista de Links"),resultado)
+    };
+}
+processaTexto(caminho);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import chalk from "chalk";
 import fs from "fs";
+import validaURL from "./http-validacao.js";
 
 function extraiLinks(texto) {
     const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
@@ -9,22 +10,22 @@ function extraiLinks(texto) {
     while ((temp = regex.exec(texto)) != null) {
         arrayResultado.push({ [temp[1]]: [temp[2]] })
     }
-    return (arrayResultado);
+    return arrayResultado.length === 0? "Não há links":arrayResultado;
 }
 
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, "Não há arquivo no caminho...."));
 }
-
-async function pegaArquivo(caminhoDoArquivo) {
+async function pegaArquivo(caminhoDoArquivo){
     const encoding = "utf-8";
 
     try {
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(extraiLinks(texto))
+        return(extraiLinks(texto))
     } catch (erro) {
         trataErro(erro);
     }
 }
-pegaArquivo('./Arquivos/texto.md');
+//pegaArquivo('./Arquivos/texto.md');
 
+export default pegaArquivo;
